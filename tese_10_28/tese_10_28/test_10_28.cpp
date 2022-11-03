5e6553a4635d75d66f0099fb616a7918e237fa13
樊文波 <2023311511@qq.com>
2022-11-03 15:26:23 +0800

    清览题库题目

---
+++ tese_10_28/tese_10_28/test_10_28.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	if (a % 3 == 0 && a % 5 == 0 && a % 7 == 0)
+		printf("%d能同时被3、5、7整除", a);
+	else if (a % 3 == 0 && a % 5 == 0)
+		printf("%d能同时被3、5整除", a);
+	else if (a % 5 == 0 && a % 7 == 0)
+		printf("%d能同时被5、7整除", a);
+	else if (a % 3 == 0 && a % 7 == 0)
+		printf("%d能同时被3、7整除", a);
+	else if (a % 3 == 0)
+		printf("%d能被3整除", a);
+	else if (a % 7 == 0)
+		printf("%d能被7整除", a);
+	else if (a % 5 == 0)
+		printf("%d能被5整除", a);
+	else printf("%d不能被3、5、7整除", a);
+	return 0;
+}
--- test_1/test_1/test_1.cpp
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include<math.h>
-int main()
-{
-	printf("hehe");
-	return 0;
-}
