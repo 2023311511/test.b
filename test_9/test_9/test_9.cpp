c3bbdb424ce305c8274b1a5afd73a8367537724d
樊文波 <2023311511@qq.com>
2022-11-21 11:10:00 +0800

    从小到大输出三个数

--- test_9/test_9/test_9.cpp
+++ test_9/test_9/test_9.cpp
@@ -1,30 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 int main()
 {
-	float a,b;
-	char op;
-	scanf("%f %f %c",&a,&b,&op);
-	if (op == '+')
-	
-		printf("%f", a+b);
-	
-	if (op == '-')
-	
-	printf("%f", a-b);
-	
-	if (op == '*')
-	
-		printf("%f", a*b);
-	
-	if (op== '/')
-	{
-		if (b == 0)
-		printf("输入错误");
-		else
-		printf("%f", a/b);
-		
-	}
-	return 0;
+    int a, b, c, t;    /*定义4个基本整型变量a、b、c、t*/
+    printf("Please input a,b,c:\n");    /*双引号内的普通字符原样输出并换行*/
+    scanf("%d %d %d", &a, &b, &c);    /*输入任意3个数*/
+    if (a > b)    /*如果a大于b,借助中间变量t实现a与b值的互换*/
+    {
+        t = a;
+        a = b;
+        b = t;
+    }
+    if (a > c)    /*如果a大于c,借助中间变景t实现a与c值的互换*/
+    {
+        t = a;
+        a = c;
+        c = t;
+    }
+    if (b > c)    /*如果b大于c,借助中间变量t实现b与c值的互换*/
+    {
+        t = b;
+        b = c;
+        c = t;
+    }
+    printf("The order of the number is:\n");
+    printf("%d,%d,%d", a, b, c);    /*输出函数顺序输出a、b、c的值*/
+    return 0;
 }
